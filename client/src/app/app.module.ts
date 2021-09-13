109fc9b7e5a46e52fc9d96dc2efe7e71dc1f9462
Boris Vasilenko <me@dreaktor.com>
2021-09-13 18:53:49 +0300

    Package ngx-translate installed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { SharedModule } from './shared/shared.module';
-import { ServicesModule } from './services/services.module';
+import { SharedModule } from './shared/shared.module'
+import { ServicesModule } from './services/services.module'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http)
+}
 
 @NgModule({
   declarations: [
@@ -20,8 +26,20 @@ import { ServicesModule } from './services/services.module';
 
     SharedModule,
     ServicesModule,
+    HttpClientModule,
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en'
+    })
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

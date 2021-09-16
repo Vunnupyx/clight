3168cbb5d9cdb72d39270b662729e4f899005669
Patrick Kopp <pk@codestryke.com>
2021-09-16 22:13:02 +0200

    Removed syntax errors

--- src/modules/Backend/routes/apis/v1/DataPoints/index.ts
+++ src/modules/Backend/routes/apis/v1/DataPoints/index.ts
@@ -1,35 +1,36 @@
-import { ConfigManager } from "../../../../../ConfigManager"
+import { ConfigManager } from "../../../../../ConfigManager";
 
 let configManager: ConfigManager;
 
 export function setConfigManger(config: ConfigManager) {
-    configManager = config;
+  configManager = config;
 }
 
 //TODO: Refactor -> One Handler for all Get calls !!!
-function dataPointsGetHandler (request, response) {
-    response.status(200).json({dataPoints: configManager?.config?.dataPoints});
+function dataPointsGetHandler(request, response) {
+  // response.status(200).json({dataPoints: configManager?.config?.dataPoints});
 }
 
-function dataPointGetHandler (request, response) {
-    const dataPoint = configManager?.config?.dataPoints.find((datapoint) => datapoint.id === request.params.id)
-    response.status(dataPoint ? 200 : 404).json(dataPoint);
+function dataPointGetHandler(request, response) {
+  // const dataPoint = configManager?.config?.dataPoints.find((datapoint) => datapoint.id === request.params.id)
+  // response.status(dataPoint ? 200 : 404).json(dataPoint);
+  response.sendStatus(500);
 }
 
-function dataPointsPostHandler (request, response) {
-    configManager?.updateConfig('dataPoints', request.body);
-    response.status(200).json();
+function dataPointsPostHandler(request, response) {
+  // configManager?.updateConfig('dataPoints', request.body);
+  response.status(200).json();
 }
-function dataPointPatchHandler (request, response) {}
-function dataPointDeleteHandler (request, response) {
-    configManager?.updateConfig('dataPoints', request.params.id);
-    response.status(200).json();
+function dataPointPatchHandler(request, response) {}
+function dataPointDeleteHandler(request, response) {
+  // configManager?.updateConfig('dataPoints', request.params.id);
+  response.status(200).json();
 }
 
 export const dataPointsHandlers = {
-    dataPointsGet: dataPointsGetHandler,
-    dataPointsPost: dataPointsPostHandler,
-    dataPointPatch: dataPointPatchHandler,
-    dataPointDelete: dataPointDeleteHandler,
-    dataPointGet: dataPointGetHandler,
-}
+  dataPointsGet: dataPointsGetHandler,
+  dataPointsPost: dataPointsPostHandler,
+  dataPointPatch: dataPointPatchHandler,
+  dataPointDelete: dataPointDeleteHandler,
+  dataPointGet: dataPointGetHandler,
+};

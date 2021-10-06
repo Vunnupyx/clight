643e482e097058577ffd610e295cd2d04d46dcd3
Markus Leitz <ml@codestryke.com>
2021-10-06 18:55:34 +0200

    test: extend test case

--- src/modules/Northbound/DataSinks/DataHubDataSink/test/DataHubDataSink.test.ts
+++ src/modules/Northbound/DataSinks/DataHubDataSink/test/DataHubDataSink.test.ts
@@ -95,70 +95,188 @@ describe('DataHubDataSink test', () => {
     });
     afterEach(() => {
       jest.clearAllMocks();
-      configMock.dataPoints = []
-      runTimeConfigMock.signalGroups.group1 = [],
-      runTimeConfigMock.signalGroups.group2 = [],
-      runTimeConfigMock.signalGroups.group3 = []
-    })
+      configMock.dataPoints = [];
+      (runTimeConfigMock.signalGroups.group1 = []),
+        (runTimeConfigMock.signalGroups.group2 = []),
+        (runTimeConfigMock.signalGroups.group3 = []);
+    });
 
     it('initializing successfully', async () => {
       return datasinkUUT.init().then(() => {
         expect(dataHubAdapterMock.init).toBeCalled();
         expect(dataHubAdapterMock.start).toBeCalled();
-        expect(winstonMock.debug).toBeCalledWith(expect.stringContaining('initializing'));
-        expect(winstonMock.debug).toBeCalledWith(expect.stringContaining('initialized'));
+        expect(winstonMock.debug).toBeCalledWith(
+          expect.stringContaining('initializing')
+        );
+        expect(winstonMock.debug).toBeCalledWith(
+          expect.stringContaining('initialized')
+        );
       });
     });
 
     it('handles measurements and sendData to adapter', async () => {
-      const mockedEvents = [{
-        measurement: {
-          id: 'testID1',
-          name: 'testName1',
-          value: 15,
-        },
-        dataSource: {
-          name: 'UTSource',
-          protocol: 'UTProtocol'
+
+      //INFO: Only 1,3,5,6 are sendable because of of twin settings.
+      const mockedEvents = [
+        {
+          measurement: {
+            id: 'testID1',
+            name: 'testName1',
+            value: 15
+          },
+          dataSource: {
+            name: 'UTSource',
+            protocol: 'UTProtocol'
+          }
         }
-      }];
+      ];
 
-      const dataPointName = 'dataPoint1';
+      const dataPointName1 = 'datapoint1';
+      const dataPointName2 = 'datapoint2';
+      const dataPointName3 = 'datapoint3';
+      const dataPointName4 = 'datapoint4';
+      const dataPointName5 = 'datapoint5';
+      const dataPointName6 = 'datapoint6';
+      const dataPointName7 = 'datapoint7';
 
-      configMock.dataPoints = [{
-        id: dataPointName,
-        type: 'probe',
-        address: 'address1',
-        name: 'name1'
-      }];
+      configMock.dataPoints = [
+        {
+          id: dataPointName1,
+          type: 'probe',
+          address: 'address',
+          name: 'name'
+        },
+        {
+          id: dataPointName2,
+          type: 'probe',
+          address: 'address',
+          name: 'name'
+        }
+        ,
+        {
+          id: dataPointName3,
+          type: 'probe',
+          address: 'address',
+          name: 'name'
+        }
+        ,
+        {
+          id: dataPointName4,
+          type: 'probe',
+          address: 'address',
+          name: 'name'
+        }
+        ,
+        {
+          id: dataPointName5,
+          type: 'probe',
+          address: 'address',
+          name: 'name'
+        }
+        ,
+        {
+          id: dataPointName6,
+          type: 'probe',
+          address: 'address',
+          name: 'name'
+        },
+        {
+          id: dataPointName7,
+          type: 'probe',
+          address: 'address',
+          name: 'name'
+        }
+      ];
 
-      dataPointMapperMock.getTargets.mockImplementation(() => [{
-        id: 'mappingID',
-        source: 'testID1',
-        target: dataPointName
-      }]);
+      dataPointMapperMock.getTargets.mockImplementation(() => [
+        {
+          id: 'mappingID',
+          source: 'testID1',
+          target: dataPointName1
+        },
+        {
+          id: 'mappingID',
+          source: 'testID1',
+          target: dataPointName2
+        },
+        {
+          id: 'mappingID',
+          source: 'testID1',
+          target: dataPointName3
+        },
+        {
+          id: 'mappingID',
+          source: 'testID1',
+          target: dataPointName4
+        },
+        {
+          id: 'mappingID',
+          source: 'testID1',
+          target: dataPointName5
+        },
+        {
+          id: 'mappingID',
+          source: 'testID1',
+          target: dataPointName6
+        },
+        {
+          id: 'mappingID',
+          source: 'testID1',
+          target: dataPointName7
+        }
+      ]);
 
       dataHubAdapterMock.getDesiredProps.mockImplementation(() => {
         return {
           services: {
             group1: {
-              enabled: true
+              enabled: true,
+              datapoint1: true,
+              datapoint2: false,
+              datapoint3: true
+            },
+            group2: {
+              enabled: false,
+              datapoint18: true,
+            },
+            group3: {
+              enabled: true,
+              datapoint4: true,
+              datapoint5: false,
+              datapoint6: true
             }
           }
-        }
+        };
       });
 
-      
-      runTimeConfigMock.signalGroups.group1 = [dataPointName];
-
-      return datasinkUUT.init()
-      .then(() => {
-        return datasinkUUT.onMeasurements(mockedEvents);
-      })
-      .then(() => {
-        expect(dataPointMapperMock.getTargets).toBeCalled();
-        expect(dataHubAdapterMock.sendData).toBeCalledWith('probe', dataPointName, mockedEvents[0].measurement.value);
-      })
-    })
+      return datasinkUUT
+        .init()
+        .then(() => {
+          return datasinkUUT.onMeasurements(mockedEvents);
+        })
+        .then(() => {
+          expect(dataPointMapperMock.getTargets).toBeCalled();
+          expect(dataHubAdapterMock.sendData).toBeCalledWith(
+            'probe',
+            dataPointName1,
+            mockedEvents[0].measurement.value
+          );
+          expect(dataHubAdapterMock.sendData).toBeCalledWith(
+            'probe',
+            dataPointName3,
+            mockedEvents[0].measurement.value
+          );
+          expect(dataHubAdapterMock.sendData).toBeCalledWith(
+            'probe',
+            dataPointName4,
+            mockedEvents[0].measurement.value
+          );
+          expect(dataHubAdapterMock.sendData).toBeCalledWith(
+            'probe',
+            dataPointName6,
+            mockedEvents[0].measurement.value
+          );
+        });
+    });
   });
 });

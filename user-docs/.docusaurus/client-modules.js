1c9bb90ac7a2d08a5cbb2efa4e05d1fcad697629
Benedikt Miller <bm@codestryke.com>
2021-10-14 15:39:05 +0200

    Docusaurus

--- user-docs/.docusaurus/client-modules.js
+++ user-docs/.docusaurus/client-modules.js
@@ -1,8 +1,8 @@
 export default [
-  require("/Users/patrickkopp/Source/mdc-light/user-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/patrickkopp/Source/mdc-light/user-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/patrickkopp/Source/mdc-light/user-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/patrickkopp/Source/mdc-light/user-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/patrickkopp/Source/mdc-light/user-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/patrickkopp/Source/mdc-light/user-docs/src/css/custom.css"),
+  require("/home/codestryke/Documents/mdc-light/user-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/codestryke/Documents/mdc-light/user-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/codestryke/Documents/mdc-light/user-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/codestryke/Documents/mdc-light/user-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/codestryke/Documents/mdc-light/user-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/codestryke/Documents/mdc-light/user-docs/src/css/custom.css"),
 ];

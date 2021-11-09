e41d6127fbf2b79b138e57d1f76f99226df03c92
Patrick Kopp <pk@codestryke.com>
2021-11-09 21:49:45 +0100

    Fixed network manager

--- services/ContainerKeys/containerkeys.sh
+++ services/ContainerKeys/containerkeys.sh
@@ -2,9 +2,9 @@
 
 KEY_COMMENT="key for network manager of mdc container"
 # Create a SSH key pair for the backend container.
-ssh-keygen -t rsa -b 4096 -N "" -C "$KEY_COMMENT" -f /root/.ssh/nmclikey
+ssh-keygen -t rsa -b 4096 -N "" -C "$KEY_COMMENT" -f /root/.ssh/nmcli/nmclikey
 # remove old keys from authorized_keys
 sed -i "/$KEY_COMMENT/d"
-# add old keys from authorized_keys
-cat /root/.ssh/nmclikey.pub >> /root/.ssh/authorized_keys
+# add new keys to authorized_keys
+cat /root/.ssh/nmcli/nmclikey.pub >> /root/.ssh/authorized_keys
 exit 0

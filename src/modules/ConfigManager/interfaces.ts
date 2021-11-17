b913797b791571353eca4bfc3ceb9fa7940849a4
Patrick Kopp <pk@codestryke.com>
2021-11-17 18:21:20 +0100

    Connected service states to backend

--- src/modules/ConfigManager/interfaces.ts
+++ src/modules/ConfigManager/interfaces.ts
@@ -113,7 +113,11 @@ export interface IDataSinkConfig {
   datahub?: IDataHubSettings;
 }
 
-export interface IDataHubDataSinkConfig extends IDataSinkConfig {}
+export interface IOpcuaAuth {
+  type: 'none' | 'userpassword';
+  userName: string;
+  password: string;
+}
 
 export interface IOpcuaAuth {
   type: 'none' | 'userpassword';
@@ -121,21 +125,6 @@ export interface IOpcuaAuth {
   password: string;
 }
 
-export interface IOpcuaDataSinkConfig extends IDataSinkConfig {
-  auth?: IOpcuaAuth;
-}
-
-export interface IDataHubDataSinkConfig extends IDataSinkConfig {}
-
-export interface IOpcuaAuth {
-  type: 'none' | 'userpassword';
-  userName: string;
-  password: string;
-}
-
-export interface IOpcuaDataSinkConfig extends IDataSinkConfig {
-  auth?: IOpcuaAuth;
-}
 export interface IDataHubConfig {
   groupDevice: boolean;
   serialNumber: string;
@@ -163,16 +152,10 @@ export interface IProxyConfig {
   enabled: boolean;
 }
 
-export interface IDataHubDataSinkConfig extends IDataSinkConfig {}
-
 export interface IOpcuaAuth {
   type: 'none' | 'userpassword';
   userName: string;
   password: string;
-}
-
-export interface IOpcuaDataSinkConfig extends IDataSinkConfig {
-  auth?: IOpcuaAuth;
 }
 
 interface IDataHubDataPointTypesData {
@@ -262,9 +245,7 @@ export interface ISystemInfo {
 
 export interface IConfig {
   dataSources: IDataSourceConfig[];
-  dataSinks: Array<
-    IDataSinkConfig | IDataHubDataSinkConfig | IOpcuaDataSinkConfig
-  >;
+  dataSinks: Array<IDataSinkConfig>;
   virtualDataPoints: IVirtualDataPointConfig[];
   mapping: IDataPointMapping[];
   general: IGeneralConfig;

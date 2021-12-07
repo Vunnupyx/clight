3463503aed029bbf8402f9ffe36d46039b0652dc
Markus Leitz <ml@codestryke.com>
2021-12-07 13:00:56 +0100

    src: remove restriction because of new required commands

--- services/ContainerKeys/containerkeys.sh
+++ services/ContainerKeys/containerkeys.sh
@@ -8,5 +8,5 @@ sed -i "/$KEY_COMMENT/d" /root/.ssh/authorized_keys
 # get container of mdc. Is also hostname
 NAME=$(docker ps -aqf "name=mdclight")
 # add new keys to authorized_keys
-cat /root/.ssh/nmcli/nmclikey.pub | sed -e `s/^/from = "$NAME" command = "nmcli"` >> /root/.ssh/authorized_keys
+cat /root/.ssh/nmcli/nmclikey.pub | sed -e `s/^/from = "$NAME"` >> /root/.ssh/authorized_keys
 exit 0

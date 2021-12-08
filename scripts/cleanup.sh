e0f1b7ef104123bf4726f531cd3451fc4cfa56cd
Patrick Kopp <pk@codestryke.com>
2021-12-08 20:00:22 +0100

    Updated cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,5 +2,7 @@ rm -rf /etc/ssh/ssh_host_* \
 /etc/MDCLight/logs/*.log \
 /etc/MDCLight/config/auth.json \
 /etc/MDCLight/config/ssl* \
+/etc/MDCLight/config/config.json \
+&& cp /etc/MDCLight/config/config.factory.json /etc/MDCLight/config/config.json \
 && cat /dev/null > ~/.bash_history \
 && history -c

6f2ce6aa86aa29475339b74b07cb7f90e95a45ab
Markus Leitz <ml@codestryke.com>
2021-12-08 11:29:41 +0100

    src: logs route implementation

--- src/modules/Backend/routes/apis/v1/Backup/index.ts
+++ src/modules/Backend/routes/apis/v1/Backup/index.ts
@@ -2,6 +2,9 @@ import { ConfigManager } from '../../../../../ConfigManager';
 import { Request, Response } from 'express';
 import winston from 'winston';
 import fs from 'fs';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+const execPromise = promisify(exec);
 
 let configManager: ConfigManager;
 
@@ -65,7 +68,63 @@ async function backupPostHandle(
   }
 }
 
+async function logsGetHandler(
+  request: Request,
+  response: Response
+): Promise<void> {
+  const logPrefix = `Backup::logsGetHandler`;
+  const date = new Date();
+  const dateString = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+  const fileName = `logs-${dateString}.zip`;
+  const path = '/mdclight/logs';
+  const zipCommand = `zip -0 -j -r ${path}/${fileName} ${path}/*.log`;
+
+  const saveDelete = () => {
+    fs.unlink(`${path}/${fileName}`, (err) => {
+      if (err) {
+        winston.error(
+          `${logPrefix} error during cleanup zip file. Retry after delay of 5sec`
+        );
+        setTimeout(saveDelete, 5 * 1000);
+        return;
+      }
+      winston.info(`${logPrefix} cleanup zip file successfully.`);
+    });
+  };
+
+  try {
+    const { stderr } = await execPromise(zipCommand);
+    if (stderr !== '') {
+      throw stderr;
+    }
+  } catch (err) {
+    winston.error(`${logPrefix} error during zip log file due to ${err}`);
+    response.status(500).send('Internal server error. Please try again later.');
+    return;
+  }
+  response.writeHead(200, {
+    'Content-Type': 'application/octet-stream',
+    'Content-Disposition': `attachment; filename=${fileName}`
+  });
+  let stream;
+  try {
+    stream = fs.createReadStream(`${path}/${fileName}`);
+  } catch (err) {
+    winston.error(`${logPrefix} error during read file due to ${err?.msg}`);
+    saveDelete();
+    response.status(500).send('Internal server error. Please try again later.');
+    return;
+  }
+  stream.on('end', () => {
+    saveDelete();
+  });
+  stream.pipe(response);
+}
+
 export const backupHandlers = {
   backupGet: backupGetHandle,
-  backupPost: backupPostHandle
+  backupPost: backupPostHandle,
+  logsGet: logsGetHandler
 };

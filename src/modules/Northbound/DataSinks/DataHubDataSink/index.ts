aa2770915ff22e85511edf01506fb9fc89757855
Markus Leitz <ml@codestryke.com>
2021-12-16 16:58:10 +0100

    src: catch invalid desired props

--- src/modules/Northbound/DataSinks/DataHubDataSink/index.ts
+++ src/modules/Northbound/DataSinks/DataHubDataSink/index.ts
@@ -78,7 +78,12 @@ export class DataHubDataSink extends DataSink {
     );
 
     const allDatapoints = [];
+    console.log(activeServices);
     activeServices.forEach((service) => {
+      if(!this.#signalGroups[service]) {
+        winston.error(`${logPrefix} received service '${service}' does not match any available service group.`);
+        return;
+      }
       this.#signalGroups[service].forEach((datapoint) => {
         allDatapoints.push(datapoint);
       });

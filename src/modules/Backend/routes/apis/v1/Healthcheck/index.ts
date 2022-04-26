9a962324954d6f5c6a9bd295baa456e553cc2314
Markus Leitz <ml@codestryke.com>
2022-04-26 16:28:43 +0200

    src: change response from version to time

--- src/modules/Backend/routes/apis/v1/Healthcheck/index.ts
+++ src/modules/Backend/routes/apis/v1/Healthcheck/index.ts
@@ -1,15 +1,7 @@
 import { Request, Response } from 'express';
 import { ConfigManager } from '../../../../../ConfigManager';
 
-let configManager: ConfigManager;
-
-/**
- * Set ConfigManager to make accessible for local function
- * @param {ConfigManager} config
- */
- export function setConfigManager(config: ConfigManager) {
-  configManager = config;
-}
+const startUpTime = Date.now();
 
 /**
  * Get System Info
@@ -17,10 +9,8 @@ let configManager: ConfigManager;
  * @param  {Response} response
  */
 async function healthCheckGetHandler(request: Request, response: Response) {
-  if(!configManager) response.status(500).json({error: "init in progress"});
-  const mdcVersion = (await configManager.getSystemInformation())[1]['items'][0]['value'];
   response.status(200).json({
-    version: mdcVersion
+    startUpTime
   });
 }
 

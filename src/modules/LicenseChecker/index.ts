24d07d90d958b5092379f60999604dfd3210cba2
Patrick Kopp <pk@codestryke.com>
2022-04-26 18:18:18 +0200

    Added codestryke devices

--- src/modules/LicenseChecker/index.ts
+++ src/modules/LicenseChecker/index.ts
@@ -1,15 +1,25 @@
-import { System } from "../System";
+import { System } from '../System';
 
 export class LicenseCheck {
-    #macList = [];
-    #system = new System();
+  #macList = [
+    // codestryke
+    '8CF31929BA4A',
+    '8CF3191EBD22',
+    '8CF3193A0A60',
+    '8CF3195B9ABE',
+    'E0DCA0C86F7E'
+  ];
+  #system = new System();
 
-    public async checkLicense(): Promise<true> {
-        const logPrefix = `${this.constructor.name}::checkMAC`;
-        const mac = await this.#system.readMacAddress('eth0');
-        if(!this.#macList.includes(mac)) {
-            throw {msg: `${logPrefix} License is not valid! Please contact support.`, code: 'LICENSE_CHECK_FAILED'};
-        }
-        return true;
+  public async checkLicense(): Promise<true> {
+    const logPrefix = `${this.constructor.name}::checkMAC`;
+    const mac = await this.#system.readMacAddress('eth0');
+    if (!this.#macList.includes(mac)) {
+      throw {
+        msg: `${logPrefix} License is not valid! Please contact support.`,
+        code: 'LICENSE_CHECK_FAILED'
+      };
     }
+    return true;
+  }
 }

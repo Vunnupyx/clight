5883622e760378aa16eed132ed6cefb0fe1d5383
Patrick Kopp <pk@codestryke.com>
2022-04-27 12:47:01 +0200

    Fixed license check on development

--- src/modules/LicenseChecker/index.ts
+++ src/modules/LicenseChecker/index.ts
@@ -16,7 +16,11 @@ export class LicenseCheck {
 
   public async checkLicense(): Promise<true> {
     const logPrefix = `${this.constructor.name}::checkMAC`;
-    const mac = await this.#system.readMacAddress('eth0');
+    let mac = await this.#system.readMacAddress('eth0');
+
+    if (mac === null && process.env.NODE_ENV === 'development')
+      mac = '00:00:00:00:00:00';
+
     if (!this.#macList.includes(mac)) {
       throw {
         msg: `${logPrefix} License is not valid! Please contact support.`,

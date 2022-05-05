51744d567a0334e97e35aa4e12c982eedfb67dbc
Patrick Kopp <pk@codestryke.com>
2022-05-05 15:44:22 +0200

    Updated cleanup scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,6 +3,7 @@
 rm -rf /etc/MDCLight/logs/*.log \
 /etc/MDCLight/config/auth.json \
 /etc/MDCLight/config/config.json \
+/etc/MDCLight/config/certs \
 && cp /etc/MDCLight/config/config.factory.json /etc/MDCLight/config/config.json \
 && cat /dev/null > ~/.bash_history \
 && history -c
--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,4 +2,5 @@
 
 rm -rf /etc/MDCLight/config/auth.json \
 /etc/MDCLight/config/config.json \
+/etc/MDCLight/config/certs \
 && cp /etc/MDCLight/config/config.factory.json /etc/MDCLight/config/config.json

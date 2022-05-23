17a0a5b2f397d2ceb6309998c99c941249a0fcc0
Markus Leitz <ml@codestryke.com>
2022-05-23 11:31:03 +0200

    update swagger files

--- src/modules/Backend/routes/swagger.ts
+++ src/modules/Backend/routes/swagger.ts
@@ -1521,7 +1521,7 @@ export default {
         },
         operationType: {
           type: 'string',
-          enum: ['and', 'or', 'not', 'counter]']
+          enum: ["and", "or", "not", "counter", "thresholds", "greater", "greaterEqual"]
         }
       }
     },

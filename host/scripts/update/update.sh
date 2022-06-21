ecb2d684fb788343ff8664fc8231990e65a48785
Markus Leitz <ml@codestryke.com>
2022-06-21 11:12:10 +0200

    script: accept env parameter

--- host/scripts/update/update.sh
+++ host/scripts/update/update.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-case $1 in 
-    dev);;
-    prod);;
-    stag);;
-    *)
-        echo "UNKNOWN ENV VARIABLE EXIT!"
-        exit 1
-esac
+# case $1 in 
+#     dev);;
+#     prod);;
+#     stag);;
+#     *)
+#         echo "UNKNOWN ENV VARIABLE EXIT!"
+#         exit 1
+# esac
 
-docker-compose -f /home/mdclite/docker-compose.yaml down &&
-docker-compose -f /home/mdclite/docker-compose.yaml --env-file .env.$1 up -d --force-recreate
+bash -c "$1 docker-compose -f /home/mdclite/docker-compose.yaml down && $1 docker-compose -f /home/mdclite/docker-compose.yaml up -d --force-recreate"

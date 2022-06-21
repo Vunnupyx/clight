7dc00ca34b00e0a21859d4561426a8ae47624734
Markus Leitz <ml@codestryke.com>
2022-06-21 11:17:58 +0200

    src: add env variables for ssh calls

--- src/modules/UpdateManager/index.ts
+++ src/modules/UpdateManager/index.ts
@@ -1,6 +1,6 @@
 import SshService from '../SshService/';
 import winston from 'winston';
-import { json } from 'body-parser';
+import { ConfigManager } from '../ConfigManager';
 
 export enum updateStatus {
   AVAILABLE = 'available',
@@ -18,6 +18,8 @@ type updateStatusType =
 export default class UpdateManager {
   #restartDelay = 10 * 1000; //ms
 
+  constructor(private configManager: ConfigManager) {}
+
   /**
    * Pull updates from remote repo.
    * If any image change restart container with new image.
@@ -25,10 +27,19 @@ export default class UpdateManager {
   triggerUpdate(): Promise<updateStatusType> {
     const logPrefix = `UpdateManager::triggerUpdate`;
 
+    // registry.gitlab.com/codestryke-tech/dmg-mori/mdc-light/
+    const selected = this.configManager.config.env.selected;  // selected environment
+    const registry = `DOCKER_REGISTRY=${this.configManager.runtimeConfig.registries[selected].url}`;
+
+    const webServerTag = `DOCKER_WEBSERVER_TAG=${this.configManager.config.env.web.tag || this.configManager.runtimeConfig.registries[selected].web.tag}`;
+    const mdcTag = `DOCKER_MDC_TAG=${this.configManager.config.env.mdc.tag || this.configManager.runtimeConfig.registries[selected].mdc.tag}`;
+    const mtcTag = `DOCKER_MTC_TAG=${this.configManager.config.env.mtc.tag || this.configManager.runtimeConfig.registries[selected].mtc.tag}`;
+
+    const envVars = `${registry} ${webServerTag} ${mdcTag} ${mtcTag}`;
     // const hostPrefix = `HOST=""`;
     const images = `docker images -q`;
-    const pull = `docker-compose pull`;
-    const restart = `screen -d -m /opt/update.sh`;
+    const pull = `${envVars} docker-compose pull`;
+    const restart = `screen -d -m /opt/update.sh "${envVars}"`; 
     const cleanup = `docker image prune -f`;
 
     let firstImages: string;
@@ -48,17 +59,18 @@ export default class UpdateManager {
       .then((response) => {
         if (response.stderr.length !== 0) throw response.stderr;
         firstImages = response.stdout;
-        winston.info(`${logPrefix} looking for available updates`);
+        winston.info(`${logPrefix} looking for available updates.`);
+        winston.debug(`${logPrefix} pull command: ${pull}`);
         return SshService.sendCommand(pull);
       })
       .then(() => {
-        winston.info(`${logPrefix} checking new images`);
+        winston.info(`${logPrefix} checking new images.`);
         return SshService.sendCommand(images);
       })
       .then((response) => {
         if (response.stderr.length !== 0) throw response.stderr;
         if (firstImages === response.stdout) {
-          winston.info(`${logPrefix} no update available. No restart required`);
+          winston.info(`${logPrefix} no update available. No restart required.`);
           return updateStatus.NOT_AVAILABLE;
         }
         // restart with delay because of response delay to frontend

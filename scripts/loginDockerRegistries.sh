6ab25aa7fbb49424b3a31948b6f2f3480c0e3598
Patrick Kopp <pk@codestryke.com>
2022-07-05 19:11:42 +0200

    Updated deploy script

--- scripts/loginDockerRegistries.sh
+++ scripts/loginDockerRegistries.sh
@@ -2,25 +2,25 @@
 # Description: Log in the remote host to all necessary registries
 
 # docker registries
-DEV=datahubdev.azurecr.io
-PROD=
-STAGE=celosnextdev.azurecr.io
+DEV=mdclightdev.azurecr.io
+PROD=mdclight.azurecr.io
+STAGE=mdclightstaging.azurecr.io
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SSHHOST=$($SCRIPTPATH/../devTools/checkEnvironmentVariable.sh)
 SSHCOMMAND="ssh $SSHHOST"
 
 # prod
-# read -p "Token for PROD registry: " TOKEN1
-# DOCKERLOGINPRODCOMMAND="docker login -u codestryke-pk-pull -p $TOKEN1 $PROD"
+read -p "Token for PROD registry: " TOKEN1
+DOCKERLOGINPRODCOMMAND="docker login -u iot-connector-flex-pull -p $TOKEN1 $PROD"
 
 # dev
 read -p "Token for DEV registry: " TOKEN2
-DOCKERLOGINDEVCOMMAND="docker login -u codestryke-pk-pull -p $TOKEN2 $DEV"
+DOCKERLOGINDEVCOMMAND="docker login -u iot-connector-flex-pull -p $TOKEN2 $DEV"
 
 # stage
 read -p "Token for STAGE registry: " TOKEN3
-DOCKERLOGINSTAGECOMMAND="docker login -u codestryke-test -p $TOKEN3 $STAGE"
+DOCKERLOGINSTAGECOMMAND="docker login -u iot-connector-flex-pull -p $TOKEN3 $STAGE"
 
 
 # chained commands

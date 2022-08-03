58b5733d72af9ca5c92f0cd5efb0e5dfbcb69814
Markus Leitz <ml@codestryke.com>
2022-08-03 19:18:08 +0200

    src: auto format

--- src/modules/SshService/index.ts
+++ src/modules/SshService/index.ts
@@ -1,4 +1,3 @@
-
 import { promisify } from 'util';
 import winston from 'winston';
 import { ChildProcess, exec } from 'child_process';
@@ -14,28 +13,37 @@ export default class SshService {
   /**
    * Sends ssh command to host
    */
-  static async sendCommand(command: string, needSudo = false, env: string[] = []): Promise<{ stdout: string | Buffer; stderr: string | Buffer; }> {
+  static async sendCommand(
+    command: string,
+    needSudo = false,
+    env: string[] = []
+  ): Promise<{ stdout: string | Buffer; stderr: string | Buffer }> {
     const logPrefix = `SshService::sendCommand`;
     const envVariables = env?.join(' ');
-    const chainedCommand = `${needSudo ? 'sudo ' : ''}${env.length > 0 ? `${envVariables} ` : ''}${command}`;
+    const chainedCommand = `${needSudo ? 'sudo ' : ''}${
+      env.length > 0 ? `${envVariables} ` : ''
+    }${command}`;
     winston.debug(`${logPrefix} sending: ${chainedCommand}`);
 
-    return execPromis(`${this.sshCommand} ${chainedCommand}`)
-      // Log exec return without changing error and response behavior
-      .then(({stderr, stdout}) => {
-        const logMsg = `${logPrefix} received from command: ${chainedCommand} \n
-        stdout: ${stdout}
-        stderr: ${stderr}
-        `;
-        winston.debug(logMsg);
-        return {stderr, stdout};
-    }).catch((err) => {
-      const logMsg = `${logPrefix} catch error from command: ${chainedCommand} \n
+    return (
+      execPromis(`${this.sshCommand} ${chainedCommand}`)
+        // Log exec return without changing error and response behavior
+        .then(({ stderr, stdout }) => {
+          const logMsg = `${logPrefix} received from command: ${chainedCommand} \n
+stdout: ${stdout}
+stderr: ${stderr}`;
+          winston.debug(logMsg);
+          return Promise.resolve({ stderr, stdout });
+        })
+        .catch((err) => {
+          const logMsg = `${logPrefix} catch error from command: ${chainedCommand} \n
         error: ${JSON.stringify(err)}
         stdout: ${err.stdout}
         stderr: ${err.stderr}
         `;
-      return Promise.reject(err);
-    });
+          winston.error(logMsg);
+          return Promise.reject(err);
+        })
+    );
   }
 }

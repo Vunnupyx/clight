ce1ab31061b2c2ca777942d69d36fb2b3189ae92
Cem Tosuner <ct@codestryke.com>
2022-11-21 21:34:44 +0100

    implements state machine to update procedure with mock transitions

--- client/src/app/pages/settings/general/update/update-dialog.component.ts
+++ client/src/app/pages/settings/general/update/update-dialog.component.ts
@@ -2,15 +2,102 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
-import {
-  SystemInformationService,
-  UpdateStatus
-} from 'app/services/system-information.service';
-import { sleep } from 'app/shared/utils';
+import { UpdateStatus } from 'app/services/system-information.service';
 import { Subscription } from 'rxjs';
+import { StateMachine } from './state-machine';
+import { StateAndTransitions } from './state-machine/interfaces';
 
 const UPDATE_POLLING_INTERVAL_MS = 1000; //10_000;
 const UPDATE_TIMEOUT_MS = 10 * 60_000;
+const UPDATE_FLOW: StateAndTransitions = {
+  INIT: {
+    transition: () =>
+      new Promise((resolve) => setTimeout(() => resolve('START_UPDATE'), 500)),
+    transitions: {
+      START_UPDATE: 'GET_UPDATES'
+    }
+  },
+  GET_UPDATES: {
+    transition: () =>
+      new Promise((resolve) => setTimeout(() => resolve('UPDATE_FOUND'), 500)),
+    transitions: {
+      UPDATE_FOUND: 'CHECK_INSTALLED_COS_VERSION',
+      NO_UPDATE_FOUND: 'END'
+    }
+  },
+  CHECK_INSTALLED_COS_VERSION: {
+    transition: () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve('VERSION_NOT_OK'), 1500)
+      ),
+    transitions: {
+      VERSION_OK: 'APPLY_MODULE_UPDATES',
+      VERSION_NOT_OK: 'CHECK_COS_UPDATES'
+    }
+  },
+  CHECK_COS_UPDATES: {
+    transition: () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve('FAILED_TO_CHECK'), 1000)
+      ),
+    transitions: {
+      UPDATE_AVAILABLE: 'APPLY_COS_UPDATES',
+      UPDATE_NOT_AVAILABLE: 'START_DOWNLOAD_COS_UPDATES'
+    }
+  },
+  START_DOWNLOAD_COS_UPDATES: {
+    transition: () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve('COS_DOWNLOAD_STARTED'), 1000)
+      ),
+    transitions: {
+      COS_DOWNLOAD_STARTED: 'VALIDATE_COS_DOWNLOAD'
+    }
+  },
+  VALIDATE_COS_DOWNLOAD: {
+    transition: () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve('COS_DOWNLOADED'), 1000)
+      ),
+    transitions: {
+      COS_DOWNLOADED: 'APPLY_COS_UPDATES'
+    }
+  },
+  APPLY_COS_UPDATES: {
+    transition: () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve('INSTALLING_COS'), 1000)
+      ),
+    transitions: {
+      INSTALLING_COS: 'WAITING_FOR_SYSTEM_RESTART'
+    }
+  },
+  WAITING_FOR_SYSTEM_RESTART: {
+    transition: () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve('SYSTEM_RESTARTED'), 1000)
+      ),
+    transitions: {
+      SYSTEM_RESTARTED: 'APPLY_MODULE_UPDATES'
+    }
+  },
+  APPLY_MODULE_UPDATES: {
+    transition: () =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve('MODULE_UPDATE_APPLIED'), 1000)
+      ),
+    transitions: {
+      MODULE_UPDATE_APPLIED: 'WAIT_FOR_MODULE_UPDATE'
+    }
+  },
+  WAIT_FOR_MODULE_UPDATE: {
+    transition: () =>
+      new Promise((resolve) => setTimeout(() => resolve('SUCCESS'), 1000)),
+    transitions: {
+      SUCCESS: 'END'
+    }
+  }
+};
 
 export interface UpdateDialogResult {
   status: UpdateStatus;
@@ -26,36 +113,21 @@ export class UpdateDialogComponent implements OnInit {
   sub = new Subscription();
   checkingForUpdates = false;
   updateInProgress = false;
+  currentState = '';
+  endReason = '';
+  stateMachine: StateMachine;
 
   constructor(
     private dialogRef: MatDialogRef<any, UpdateDialogResult>,
     @Inject(MAT_DIALOG_DATA) private data: any,
-    private systemInformationService: SystemInformationService,
     private translate: TranslateService
   ) {}
 
-  async ngOnInit(): Promise<void> {
-    try {
-      this.checkingForUpdates = true;
-      const { startUpTime } = await this.systemInformationService.healthcheck();
-      const status = await this.systemInformationService.getUpdateStatus();
-      this.checkingForUpdates = false;
-
-      if (status === UpdateStatus.UpToDate) {
-        this.dialogRef.close({ status });
-      }
-
-      if (status === UpdateStatus.NeedsUpdate) {
-        this.updateInProgress = true;
-        await this.waitForUpdateComplete(startUpTime);
-        this.dialogRef.close({ status: UpdateStatus.UpdateSuccessful });
-      }
-    } catch (err: any) {
+  async ngOnInit(): Promise<any> {
+    Promise.race([this.checkTimeout(), this.update()]).catch((err) => {
       console.error(err);
 
-      let errorText = this.translate.instant(
-        'system-information.UpdateFailedCheckNetworkConfig'
-      );
+      let errorText = '';
       if (err instanceof HttpErrorResponse && err.error) {
         try {
           const { error, code } = JSON.parse(err.error);
@@ -64,39 +136,53 @@ export class UpdateDialogComponent implements OnInit {
             : error;
         } catch (e) {
           this.dialogRef.close({
-            status: UpdateStatus.UnexpectedError
+            status: UpdateStatus.ERROR
           });
         }
-      } else if (err?.error?.message) {
-        errorText = err.error.message;
+      } else {
+        errorText = this.endReason;
       }
       this.dialogRef.close({
-        status: UpdateStatus.CheckFailed,
-        error: errorText
+        status: UpdateStatus.ERROR,
+        error: this.endReason
       });
-    }
+    });
   }
 
-  private async waitForUpdateComplete(systemStartTime: string) {
-    const started = Date.now();
+  private async update(): Promise<void> {
+    this.checkingForUpdates = true;
+    this.checkTimeout();
 
-    while (!(await this.checkVersionChanged(systemStartTime))) {
-      if (Date.now() - started > UPDATE_TIMEOUT_MS) {
-        throw new Error(
-          this.translate.instant('system-information.UpdateInstallFailed')
-        );
+    const onStateChange = (newState: string, endReason?: string) => {
+      this.currentState = newState;
+      this.endReason = endReason;
+      if (this.currentState === 'END') {
+        this.checkingForUpdates = false;
+        if (this.endReason === 'SUCCESS') {
+          this.dialogRef.close({ status: UpdateStatus.SUCCESS });
+        } else if (this.endReason === 'NO_UPDATE_FOUND') {
+          this.dialogRef.close({ status: UpdateStatus.UP_TO_DATE });
+        } else {
+          this.dialogRef.close({
+            status: UpdateStatus.ERROR,
+            error: this.endReason //TBD different error cases
+          });
+        }
+      } else if (this.currentState === 'CHECK_INSTALLED_COS_VERSION') {
+        this.updateInProgress = true;
       }
+    };
+    this.stateMachine = new StateMachine(UPDATE_FLOW, onStateChange);
 
-      await sleep(UPDATE_POLLING_INTERVAL_MS);
-    }
+    await this.stateMachine.start();
   }
 
-  private async checkVersionChanged(systemStartTime: string) {
-    try {
-      const { startUpTime } = await this.systemInformationService.healthcheck();
-      return startUpTime != systemStartTime;
-    } catch (err) {
-      return false;
-    }
+  private async checkTimeout() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        this.endReason = 'ERROR_TIMEOUT';
+        reject();
+      }, UPDATE_TIMEOUT_MS);
+    });
   }
 }

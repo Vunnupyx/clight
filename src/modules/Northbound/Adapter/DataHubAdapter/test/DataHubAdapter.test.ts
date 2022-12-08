c695bb81dbb6e02127f795e92339f2bbaad881f0
Cem Tosuner <ct@codestryke.com>
2022-12-08 18:31:34 +0100

    updates and fixes datahubadapter test cases

--- src/modules/Northbound/Adapter/DataHubAdapter/test/DataHubAdapter.test.ts
+++ src/modules/Northbound/Adapter/DataHubAdapter/test/DataHubAdapter.test.ts
@@ -1,14 +1,11 @@
+import { DataHubAdapter } from '../';
+import { LifecycleEventStatus } from '../../../../../common/interfaces';
+
 let UUT: DataHubAdapter;
 const proxyMock = {
   enabled: true
 };
 const signalGroupsMock = {};
-const dataHubSettingsMock = {
-  provisioningHost: 'dummy.host.codestryke',
-  scopeId: 'dummy scopeId',
-  regId: 'dummy regid',
-  symKey: 'dummy key'
-};
 const dataHubAdapterOptionsMock = {
   groupDevice: false,
   signalGroups: signalGroupsMock,
@@ -25,7 +22,13 @@ const dataHubAdapterOptionsMock = {
 
 const twinMock = {
   on: jest.fn(),
+  shutdown: jest.fn(),
   properties: {
+    desired: {
+      services: {
+        key1: 'value1'
+      }
+    },
     reported: {
       update: jest.fn((payload, callback) => callback())
     }
@@ -42,6 +45,9 @@ const dataHubClientMock = {
   on: jest.fn().mockImplementation(() => {
     return dataHubClientMock;
   }),
+  close: jest.fn().mockImplementation(() => {
+    return Promise.resolve({});
+  }),
   setOptions: jest.fn(),
   sendEvent: jest.fn(),
   open: jest.fn().mockImplementation(() => {
@@ -50,23 +56,32 @@ const dataHubClientMock = {
   getTwin: jest.fn().mockReturnValue(twinMock)
 };
 
-const logFunc = (msg) => {
-  // console.log(msg);
-};
-const winstonMock = {
-  winston: jest.fn(), // Constructor
-  debug: jest.fn(logFunc),
-  info: jest.fn(logFunc),
-  warn: jest.fn(logFunc),
-  error: jest.fn(logFunc)
-};
-jest.doMock('winston', () => {
-  return winstonMock;
+function log(m) {
+  console.log(m);
+}
+
+jest.mock('winston', () => {
+  return {
+    debug: log,
+    info: log,
+    error: log,
+    warn: log
+  };
 });
 
 jest.doMock('url');
 jest.doMock('crypto');
-jest.doMock('azure-iot-device');
+jest.createMockFromModule('azure-iot-device');
+jest.mock('azure-iot-device', () => {
+  return {
+    Message: jest.fn(() => ({ properties: { add: jest.fn() } })),
+    Twin: jest.fn(),
+    ModuleClient: {
+      fromEnvironment: jest.fn(() => dataHubClientMock)
+    }
+  };
+});
+jest.doMock('azure-iot-device-mqtt');
 jest.doMock('https-proxy-agent');
 jest.doMock('socks-proxy-agent');
 
@@ -74,45 +89,18 @@ jest.doMock('azure-iot-security-symmetric-key');
 jest.doMock('azure-iot-provisioning-device-amqp');
 jest.doMock('azure-iot-provisioning-device/dist/interfaces');
 
-import { DataHubAdapter } from '../';
-import { ProvisioningDeviceClient } from 'azure-iot-provisioning-device';
-import { Client } from 'azure-iot-device';
-
-const clientSpy = jest
-  .spyOn(Client, 'fromConnectionString')
-  .mockImplementation(() => {
-    return dataHubClientMock as any;
-  });
-
-const ProvisioningDeviceClientSpy = jest
-  .spyOn(ProvisioningDeviceClient, 'create')
-  .mockImplementation(() => ({
-    register: jest.fn().mockImplementation((cb) => {
-      cb(null, deviceRegistrationStateMock);
-    }),
-    cancel: jest.fn(),
-    setProvisioningPayload: jest.fn()
-  }));
+const onStateChange = jest.fn();
 
 jest.useFakeTimers();
 
 // All tests in this block depends on each other. Not single runnable.
 describe('DataHubAdapter', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-    jest.resetAllMocks();
-  });
-  beforeAll(async () => {
-    UUT = new DataHubAdapter(dataHubAdapterOptionsMock, dataHubSettingsMock, {
-      type: 'http',
-      ip: '',
-      port: 0,
-      enabled: true
-    });
+  beforeEach(async () => {
+    UUT = new DataHubAdapter(dataHubAdapterOptionsMock, onStateChange);
     await UUT.init().then((adapter) => adapter.start());
   });
-  it('reported properties are reported', async () => {
-    await UUT.setReportedProps({
+  test('setReportedProps reports properties', async () => {
+    const obj = {
       reported1: {
         enabled: true
       },
@@ -128,26 +116,81 @@ describe('DataHubAdapter', () => {
       reported5: {
         enabled: true
       }
-    });
+    };
+    await UUT.setReportedProps(obj);
+    expect(UUT.running).toBe(true);
+    expect(onStateChange).toHaveBeenCalledWith(LifecycleEventStatus.Connected);
     expect(twinMock.properties.reported.update).toHaveBeenCalled();
     expect(twinMock.properties.reported.update.mock.calls[0][0]).toStrictEqual({
-      services: {
-        reported1: {
-          enabled: true
-        },
-        reported2: {
-          enabled: true
-        },
-        reported3: {
-          enabled: true
-        },
-        reported4: {
-          enabled: true
-        },
-        reported5: {
-          enabled: true
-        }
-      }
+      services: obj
     });
+  });
+
+  test('getReportedProps gets desired properties', async () => {
+    let response = await UUT.getDesiredProps();
+    expect(UUT.running).toBe(true);
+    expect(response).toStrictEqual({
+      services: { key1: 'value1' }
+    });
+  });
+
+  test('sends data correctly with no measurements', async () => {
+    const measurement = {
+      event: [],
+      probe: [],
+      telemetry: []
+    };
+    await UUT.sendData(measurement);
+    expect(UUT.running).toBe(true);
+  });
+
+  test('sends data correctly with all measurements', async () => {
+    const measurement = {
+      event: [
+        {
+          example: ''
+        }
+      ],
+      probe: [
+        {
+          example: ''
+        }
+      ],
+      telemetry: [
+        {
+          example: ''
+        }
+      ]
+    };
+    await UUT.sendData(measurement);
+    expect(UUT.running).toBe(true);
+    expect(dataHubClientMock.sendEvent).toHaveBeenNthCalledWith(
+      1,
+      expect.objectContaining({ properties: { add: expect.any(Function) } }),
+      expect.any(Function)
+    );
+    expect(dataHubClientMock.sendEvent).toHaveBeenNthCalledWith(
+      2,
+      expect.objectContaining({ properties: { add: expect.any(Function) } }),
+      expect.any(Function)
+    );
+    expect(dataHubClientMock.sendEvent).toHaveBeenNthCalledWith(
+      3,
+      expect.objectContaining({ properties: { add: expect.any(Function) } }),
+      expect.any(Function)
+    );
+  });
+
+  test('stopping works correctly', async () => {
+    await UUT.stop();
+    expect(UUT.running).toBe(undefined); //not false, because this.isRunning is deleted in stop process
+    expect(onStateChange).toHaveBeenCalledWith(
+      LifecycleEventStatus.Disconnected
+    );
+  });
+  test('shutdown works correctly', async () => {
+    await UUT.shutdown();
+    expect(twinMock.shutdown).toHaveBeenCalled();
+    expect(dataHubClientMock.close).toHaveBeenCalled();
   });
 });

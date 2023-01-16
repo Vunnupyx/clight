fe1365d8212f102d42b4d54de6953408b5acf6be
Cem Tosuner <ct@codestryke.com>
2023-01-16 12:12:33 +0100

    description added to livedata type

--- client/src/app/models/livedata.ts
+++ client/src/app/models/livedata.ts
@@ -7,6 +7,7 @@ export class DataPointLiveData {
   dataPointId!: string;
   value!: string | number | boolean;
   unit?: string;
+  description?: string;
   timestamp!: number;
   timeseries?: TimeSeriesValue[];
 }

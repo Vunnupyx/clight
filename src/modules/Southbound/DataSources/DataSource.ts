e292d4b02f1b0f3de28a75737ac62948a6711595
Cem Tosuner <ct@codestryke.com>
2023-01-23 13:38:29 +0100

    removes unnecessary abstract method

--- src/modules/Southbound/DataSources/DataSource.ts
+++ src/modules/Southbound/DataSources/DataSource.ts
@@ -259,9 +259,4 @@ export abstract class DataSource extends EventEmitter {
   public getCurrentStatus(): LifecycleEventStatus {
     return this.currentStatus;
   }
-
-  /**
-   * Returns current tariff number for energy data source
-   */
-  public abstract getCurrentTariffNumber(): string;
 }

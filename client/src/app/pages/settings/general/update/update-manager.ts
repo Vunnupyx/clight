6d20a24a51bf8c88b4f2da73436bdb100ddeb68b
Cem Tosuner <ct@codestryke.com>
2023-01-25 16:15:16 +0100

    updates semver check to include rc format

--- client/src/app/pages/settings/general/update/update-manager.ts
+++ client/src/app/pages/settings/general/update/update-manager.ts
@@ -492,6 +492,7 @@ export class UpdateManager {
   /**
    * Compares two semantic versions to determine which one is newer.
    * Returns 1 if second > first, 0 if equal and -1 if first > second.
+   * Supported semver formats: x.x.x , x.x.x-rcx or x.x.x-x-hash
    * Returns null if error
    * @param first
    * @param second
@@ -502,15 +503,27 @@ export class UpdateManager {
     second: string
   ): 1 | 0 | -1 | null {
     try {
-      const [major1, minor1, patch1] = first?.split('-')?.[0]?.split('.');
-      const [major2, minor2, patch2] = second?.split('-')?.[0]?.split('.');
+      const [major1, minor1, patch1, rc1] = first
+        ?.replace('-', '.')
+        ?.replace('rc', '')
+        ?.split('.');
+      const [major2, minor2, patch2, rc2] = second
+        ?.replace('-', '.')
+        ?.replace('rc', '')
+        ?.split('.');
 
       if (
         parseInt(major2, 10) > parseInt(major1, 10) ||
         (major2 === major1 && parseInt(minor2, 10) > parseInt(minor1, 10)) ||
         (major2 === major1 &&
           minor2 === minor1 &&
-          parseInt(patch2, 10) > parseInt(patch1, 10))
+          parseInt(patch2, 10) > parseInt(patch1, 10)) ||
+        (major2 === major1 &&
+          minor2 === minor1 &&
+          patch2 === patch1 &&
+          typeof rc1 !== 'undefined' &&
+          typeof rc2 !== 'undefined' &&
+          parseInt(rc2, 10) > parseInt(rc1, 10))
       ) {
         return 1;
       }
@@ -520,7 +533,13 @@ export class UpdateManager {
         (major1 === major2 && parseInt(minor1, 10) > parseInt(minor2, 10)) ||
         (major1 === major2 &&
           minor1 === minor2 &&
-          parseInt(patch1, 10) > parseInt(patch2, 10))
+          parseInt(patch1, 10) > parseInt(patch2, 10)) ||
+        (major1 === major2 &&
+          minor1 === minor2 &&
+          patch1 === patch2 &&
+          typeof rc1 !== 'undefined' &&
+          typeof rc2 !== 'undefined' &&
+          parseInt(rc1, 10) > parseInt(rc2, 10))
       ) {
         return -1;
       }

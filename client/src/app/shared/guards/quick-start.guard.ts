c0815e0df98b93230dff1a100c38e8c19393eb6b
Vunnupyx <muhutdinovrifat@yandex.ru>
2023-02-01 18:50:35 +0400

    feat: add guard for quick start

--- client/src/app/shared/guards/quick-start.guard.ts
+++ client/src/app/shared/guards/quick-start.guard.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
 
-import { TemplateService } from '../../services';
+import { TemplateService, TermsAndConditionsService } from 'app/services';
+import { AuthService } from '../services';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +17,24 @@ export class QuickStartGuard implements CanActivate {
   constructor(
     private router: Router,
     private templateService: TemplateService,
+    private termsService: TermsAndConditionsService,
+    private authService: AuthService
   ) {}
 
   async canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Promise<boolean | UrlTree> {
-    const isCompleted = await this.templateService.isCompleted();
+    state: RouterStateSnapshot
+  ): Promise<boolean | UrlTree> {
+    const isLoggedIn = !!this.authService.token;
+    if (isLoggedIn) {
+      const isCompleted = await this.templateService.isCompleted();
+      const isTermsAccepted = await this.termsService.isAccepted();
 
-    if (!isCompleted) {
-      if (route.routeConfig?.path === 'quick-start') {
+      if (isCompleted && isTermsAccepted) {
         return true;
       }
-
-      return this.router.navigate(['/quick-start']);
     }
 
-    return this.router.navigate(['/settings/general']);
+    return this.router.navigate(['/quick-start']).then(() => false);
   }
 }

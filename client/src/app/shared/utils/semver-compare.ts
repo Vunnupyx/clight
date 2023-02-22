f03d802b5ab080943df636a32db29515a72364f2
Cem Tosuner <ct@codestryke.com>
2023-02-22 17:44:19 +0100

    updates semantic versioning check logic

--- client/src/app/shared/utils/semver-compare.ts
+++ client/src/app/shared/utils/semver-compare.ts
@@ -1,60 +1,110 @@
+import { rcompare, valid } from 'semver';
 /**
  * Compares two semantic versions to determine which one is newer.
- * Returns 1 if second > first, 0 if equal and -1 if first > second.
- * Supported semver formats: x.x.x , x.x.x-rcx or x.x.x-x-hash
- * Returns null if error
- * @param first
- * @param second
- * @returns
+ * Supported semver format: <semver1>-iot2050-<semver2>
+ *
+ * @param input1
+ * @param input2
+ * @returns 1 if second > first, 0 if equal and -1 if first > second. Returns null if error
  */
-export function compareVersionSemver(
-  first: string,
-  second: string
+export function compareSemanticVersion(
+  input1: string,
+  input2: string
 ): 1 | 0 | -1 | null {
   try {
-    const [major1, minor1, patch1, rc1] = first
-      ?.replace('-', '.')
-      ?.replace('rc', '')
-      ?.split('.');
-    const [major2, minor2, patch2, rc2] = second
-      ?.replace('-', '.')
-      ?.replace('rc', '')
-      ?.split('.');
-
-    if (
-      first?.length === 0 ||
-      parseInt(major2, 10) > parseInt(major1, 10) ||
-      (major2 === major1 && parseInt(minor2, 10) > parseInt(minor1, 10)) ||
-      (major2 === major1 &&
-        minor2 === minor1 &&
-        parseInt(patch2, 10) > parseInt(patch1, 10)) ||
-      (major2 === major1 &&
-        minor2 === minor1 &&
-        patch2 === patch1 &&
-        (typeof rc1 !== 'undefined' || typeof rc2 !== 'undefined') &&
-        (typeof rc1 === 'undefined' || parseInt(rc2, 10) > parseInt(rc1, 10)))
-    ) {
-      return 1;
+    /**
+     * If both inputs are not valid then return null
+     */
+    if (!valid(input1) && !valid(input2)) {
+      return null;
     }
 
-    if (
-      second?.length === 0 ||
-      parseInt(major1, 10) > parseInt(major2, 10) ||
-      (major1 === major2 && parseInt(minor1, 10) > parseInt(minor2, 10)) ||
-      (major1 === major2 &&
-        minor1 === minor2 &&
-        parseInt(patch1, 10) > parseInt(patch2, 10)) ||
-      (major1 === major2 &&
-        minor1 === minor2 &&
-        patch1 === patch2 &&
-        (typeof rc1 !== 'undefined' || typeof rc2 !== 'undefined') &&
-        (typeof rc2 === 'undefined' || parseInt(rc1, 10) > parseInt(rc2, 10)))
-    ) {
+    /**
+     * If one of the input is invalid but the other one is valid, return the valid one
+     */
+    if (!valid(input1) && valid(input2)) {
+      return 1;
+    } else if (valid(input1) && !valid(input2)) {
       return -1;
     }
 
-    return 0;
-  } catch (e) {
+    let [input1FirstPart, input1SecondPart = '0.0.0'] =
+      input1.split('-iot2050-');
+    let [input2FirstPart, input2SecondPart = '0.0.0'] =
+      input2.split('-iot2050-');
+
+    let compareResult = rcompare(input1FirstPart, input2FirstPart);
+
+    /**
+     * Check <semver1> part of the given format "<semver1>-iot2050-<semver2>"
+     */
+    if (input1FirstPart.includes('-rc') || input2SecondPart.includes('-rc')) {
+      let [input1VersionWithoutRC, input1RcVersion] =
+        input1FirstPart.split('-rc');
+      let [input2VersionWithoutRC, input2RcVersion] =
+        input2FirstPart.split('-rc');
+
+      compareResult = rcompare(input1VersionWithoutRC, input2VersionWithoutRC);
+      if (compareResult === 0) {
+        if (input1RcVersion && !input2RcVersion) {
+          //If main versions are the same and one has RC and the other does not have RC, then the one without RC is newer
+          compareResult = 1;
+        } else if (!input1RcVersion && input2RcVersion) {
+          //If main versions are the same and one has RC and the other does not have RC, then the one without RC is newer
+          compareResult = -1;
+        } else {
+          /**
+           * Both has RC versions, they need to be compared
+           *
+           * Parse RC versions, and remove if there is any dot -> this is the case if for example -rc.9 format used.
+           * If one does not have RC version, -1 is used, so that comparison later can function correctly.
+           *  Example: If one has RC and other one does not and the main versions are the same, then rc one is older
+           */
+
+          const parsedInput1RcVersion = parseInt(
+            input1RcVersion?.replace('.', '') ?? '-1',
+            10
+          );
+          const parsedInput2RcVersion = parseInt(
+            input2RcVersion?.replace('.', '') ?? '-1',
+            10
+          );
+
+          if (parsedInput1RcVersion > parsedInput2RcVersion) {
+            compareResult = -1;
+          } else if (parsedInput1RcVersion < parsedInput2RcVersion) {
+            compareResult = 1;
+          } else {
+            compareResult = 0;
+          }
+        }
+      }
+    }
+
+    /**
+     * Check <semver2> part of the given format "<semver1>-iot2050-<semver2>" if first part was equal
+     */
+    if (compareResult === 0) {
+      if (input1SecondPart.includes('.') && input2SecondPart.includes('.')) {
+        // If both has x.x.x format, then it can be compared as normal semver
+        compareResult = rcompare(input1SecondPart, input2SecondPart);
+      } else if (
+        // If one of them has only a number then compare them as integer
+        parseInt(input1SecondPart, 10) > parseInt(input2SecondPart, 10)
+      ) {
+        compareResult = -1;
+      } else if (
+        // If one of them has only a number then compare them as integer
+        parseInt(input1SecondPart, 10) < parseInt(input2SecondPart, 10)
+      ) {
+        compareResult = 1;
+      } else {
+        compareResult = 0;
+      }
+    }
+
+    return compareResult;
+  } catch (err) {
     return null;
   }
 }
--- client/src/app/shared/utils/test/semver-compare.test.ts
+++ client/src/app/shared/utils/test/semver-compare.test.ts
@@ -1,6 +1,6 @@
-import { compareVersionSemver } from '..';
+import { compareSemanticVersion } from '..';
 
-describe('Semver Comparison', () => {
+describe('Semantic Version Comparison', () => {
   // Result 1 means second one is newer, result -1 means first one is newer, 0 means both are equal
   test.each([
     {
@@ -14,9 +14,84 @@ describe('Semver Comparison', () => {
       expectedResult: 0
     },
     {
+      first: '1.9.7-rc1-iot2050-r0',
+      second: '1.9.7',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-rc1-iot2050-0',
+      second: '1.9.7-rc2-iot2050-0',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-rc1-iot2050-9',
+      second: '1.9.7-rc1-iot2050-10',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-rc.1-iot2050-9',
+      second: '1.9.7-rc.1-iot2050-10',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-iot2050-1',
+      second: '1.9.7-rc1-iot2050-1',
+      expectedResult: -1
+    },
+    {
+      first: '1.9.7-iot2050-1',
+      second: '1.9.7-rc.1-iot2050-1',
+      expectedResult: -1
+    },
+    {
+      first: '1.9.7-rc1-iot2050-9',
+      second: '1.9.7-rc2-iot2050-1',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-rc9-iot2050-0',
+      second: '1.9.7-rc10-iot2050-0',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-rc.9-iot2050-0',
+      second: '1.9.7-rc.10-iot2050-0',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-iot2050-1',
+      second: '1.9.8-rc1-iot2050-1',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-iot2050-1',
+      second: '1.9.8-rc1-iot2050-1',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-gi5ghz4',
+      second: '1.9.7',
+      expectedResult: 1
+    },
+    {
+      first: '1.9.7-iot2050-0.2.1',
+      second: '1.9.7-iot2050-0.0.1',
+      expectedResult: -1
+    },
+    {
+      first: '1.9.7-iot2050-0',
+      second: '1.9.7-iot2050-0.0.1',
+      expectedResult: 0 // cannot compare 0 vs 0.0.1 as semver, so second one is parsed as integer
+    },
+    {
+      first: '1.9.7-gi5ghz4',
+      second: '1.9.7-g293573',
+      expectedResult: -1 //string comparison of the hash
+    },
+    {
       first: '3.0.0-5-g6h76',
       second: '3.0.0-5-tkgj6',
-      expectedResult: 0
+      expectedResult: 1 //string comparison of the hash
     },
     {
       first: '0.0.1',
@@ -40,17 +115,12 @@ describe('Semver Comparison', () => {
     },
     {
       first: '3.0.4',
-      second: '3.0.4-rc0',
-      expectedResult: 1
-    },
-    {
-      first: '3.0.4',
       second: '3.0.4-rc1',
-      expectedResult: 1
+      expectedResult: -1
     },
     {
-      first: '3.0.4-rc9',
-      second: '3.0.4-rc10',
+      first: '3.0.4-rc.9',
+      second: '3.0.4-rc.10',
       expectedResult: 1
     },
     {
@@ -69,20 +139,55 @@ describe('Semver Comparison', () => {
       expectedResult: -1
     },
     {
+      first: '3',
+      second: '3.0.0',
+      expectedResult: 1
+    },
+    {
+      first: '3.0.0',
+      second: '4',
+      expectedResult: -1
+    },
+    {
+      first: '3.0.0',
+      second: '3.0',
+      expectedResult: -1
+    },
+    {
       first: undefined,
       second: '3.0.4',
-      expectedResult: null
+      expectedResult: 1
+    },
+    {
+      first: 3,
+      second: '3.0.0',
+      expectedResult: 1
+    },
+    {
+      first: { v: '3.0.0' },
+      second: '3.0.0',
+      expectedResult: 1
     },
     {
       first: undefined,
       second: undefined,
       expectedResult: null
+    },
+    {
+      first: '',
+      second: undefined,
+      expectedResult: null
+    },
+    {
+      first: '',
+      second: '',
+      expectedResult: null
     }
   ])(
-    '$first and $second -> Newer version should be: $expectedResult',
+    '$first and $second -> result should be: $expectedResult',
     ({ first, second, expectedResult }) => {
       //@ts-ignore
-      const result = compareVersionSemver(first, second);
+      const result = compareSemanticVersion(first, second);
       //@ts-ignore
       expect(result).toBe(expectedResult);
     }

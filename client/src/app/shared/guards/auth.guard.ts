1f563bf8a1e4296ff44460df50d0bf7a10ca9357
Vunnupyx <muhutdinovrifat@yandex.ru>
2023-04-12 21:21:36 +0400

    feat: redirect user if they are logged

--- client/src/app/shared/guards/auth.guard.ts
+++ client/src/app/shared/guards/auth.guard.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
 
-import { TemplateService } from '../../services';
-import {AuthService} from "../services";
+import { AuthService } from '../services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-  ) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   async canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Promise<boolean | UrlTree> {
+    state: RouterStateSnapshot
+  ): Promise<boolean | UrlTree> {
     const isLoggedIn = !!this.authService.token;
 
     if (isLoggedIn) {
+      if (route.routeConfig?.path === 'login')
+        return this.router.navigate(['/']);
+
       return true;
     }
+    if (route.routeConfig?.path === 'login') return true;
 
     return this.router.navigate(['/login']).then(() => false);
   }

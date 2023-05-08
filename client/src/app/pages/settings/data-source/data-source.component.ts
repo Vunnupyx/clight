707318111c464f636791f9994c7f27989a146d51
Boris Vasilenko <me@dreaktor.com>
2023-05-08 07:55:27 +0300

    allow data source with IP, hostname, or both

--- client/src/app/pages/settings/data-source/data-source.component.ts
+++ client/src/app/pages/settings/data-source/data-source.component.ts
@@ -71,7 +71,6 @@ export class DataSourceComponent implements OnInit, OnDestroy {
   liveDataSub!: Subscription;
   statusSub!: Subscription;
 
-  ipRegex = IP_REGEX;
   portRegex = PORT_REGEX;
   ipOrHostRegex = `${IP_REGEX}|${HOST_REGEX}`;
   dsFormValid = true;
